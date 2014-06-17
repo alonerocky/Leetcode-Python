28fa24678e16ec82aafa038d36bc332c72c25167
shoulong li <lishoulong@gmail.com>
2014-06-16 23:02:44 -0700

    is same tree

--- src/TreeNode.py
+++ src/TreeNode.py
@@ -24,3 +24,13 @@ class TreeNode(object):
         left = self.getHeight(root.left)
         right = self.getHeight(root.right)
         return max(left,right) + 1
+
+    def isSameTree(self, p, q):
+        if p == q:
+            return True
+        if p == None and q == None:
+            return True
+        elif p != None and q != None:
+            return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        else:
+            return False
