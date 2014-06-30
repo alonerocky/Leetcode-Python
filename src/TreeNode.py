a37dc0caa62c963cab1925369b11bf638d5c58ea
shoulong li <lishoulong@gmail.com>
2014-06-30 00:08:31 -0700

    construct binary tree and change list.txt

--- src/TreeNode.py
+++ src/TreeNode.py
@@ -65,5 +65,90 @@ class TreeNode(object):
         result.right = self.sortedArrayToBST_helper(num,middle + 1, end)
         return result
 
+    # @param inorder, a list of integers
+    # @param postorder, a list of integers
+    # @return a tree node
+    def buildTree_inorder_poster(self, inorder, postorder):
+        return self.buildTree_inorder_poster_helper(inorder,0,len(inorder) -1, \
+                                                    postorder,0,len(postorder)-1)
 
+    def buildTree_inorder_poster_helper(self,inorder, istart,iend,postorder,pstart,pend):
+        if (inorder == None or \
+            len(inorder) == 0 or \
+            postorder == None or \
+            len(postorder) == 0 or \
+            istart > iend or \
+            pstart > pend or \
+            iend - istart != pend - pstart \
+            ):
+            return None
+        root  = postorder[pend]
+        result = TreeNode(root)
+        # postorder: [pstart,  pend -1]
+        # find index in inorder array
+        index = self.getIndex(inorder,istart,iend,root)
+        if index == -1:
+            return result
+        # inorder[index] == root
+        # istart, index -1 : left
+        # index + 1 , iend : right
+
+        # pstart,  x : left
+        # x +1,   pend - 1: right
+
+        # x: x  - pstart = index -1 - istart
+        #    x = index -1 + pstart - istart
+        # pend -1 - x -1 = iend - index-1
+        # x = pend - iend + index -1
+        pindex = index -1 + pstart - istart
+        result.left = self.buildTree_inorder_poster_helper(inorder, istart, index -1,\
+                                                           postorder, pstart, pindex )
+        result.right = self.buildTree_inorder_poster_helper(inorder, index + 1, iend, \
+                                                            postorder, pindex + 1, pend -1)
+
+
+        return result
+
+    def getIndex(self, array, start, end,target):
+        if array == None or start > end :
+            return -1
+        for i in range(start,end + 1):
+            if array[i] == target:
+                return i
+        return -1
+
+    # @param preorder, a list of integers
+    # @param inorder, a list of integers
+    # @return a tree node
+    def buildTree_preorder_inorder(self, preorder, inorder):
+        return self.buildTree_preorder_inorder_helper(preorder,0,len(preorder) -1, inorder,0,len(inorder) -1)
+    def buildTree_preorder_inorder_helper(self,preorder,pstart,pend,inorder,istart,iend):
+        if preorder == None or \
+        len(preorder) == 0 or \
+        inorder == None or \
+        len(inorder) == 0 or \
+        pstart > pend or \
+        istart > iend or \
+        pend - pstart != iend - istart:
+            return None
+        root = preorder[pstart]
+        result = TreeNode(root)
+        # [pstart + 1, pindex] : left
+        # [pindex + 1, pend]   : right
+        index = self.getIndex(inorder, istart, iend, root)
+        if index == -1:
+            return result
+        # istart, index - 1: left
+        # index + 1, iend:   right
+        # pindex - pstart -1 = index - 1 - istart
+        pindex = pstart - istart + index
+        # pend - pindex - 1 = iend - index -1
+        result.left = self.buildTree_preorder_inorder_helper(preorder, pstart + 1, pindex, inorder, istart, index -1)
+        result.right = self.buildTree_preorder_inorder_helper(preorder, pindex + 1, pend, inorder, index + 1, iend)
+        return result
+
+inorder = [1,2]
+postorder = [2,1]
+root = TreeNode(0)
+root.buildTree_inorder_poster(inorder,postorder)
 
